e50a88cce16ab4d909baed912a8a154e2847c58e
Donald McPartland <blargalicious@gmail.com>
2022-06-21 20:20:16 -0700

    create a canvas that can draw random nodes

--- breadth-first-search.js
+++ breadth-first-search.js
@@ -1,3 +1,47 @@
-function showAlert() {
-    alert("Hello Dax");
+var numberOfNodes = 0;
+
+function createNode() {
+    var c = document.getElementById("bfs-canvas");
+    var ctx = c.getContext("2d");
+
+    //random number between 0 and 480...which is a rough boundary of our canvas that's 500x500
+    const x = Math.floor(Math.random() * 480);
+    const y = Math.floor(Math.random() * 480);
+
+    var node = new Node(x, y, numberOfNodes++);
+    node.drawSelf(ctx);
+}
+
+
+
+class Node {
+    constructor(x, y, text) {
+        this.x = x;
+        this.y = y;
+        this.text = text;
+    }
+
+    drawSelf(canvasContext) {
+        this.drawCircle(canvasContext);
+        this.drawText(canvasContext);
+    }
+
+    drawCircle(context) {
+        context.beginPath();
+
+        context.arc(this.x, this.y, 20, 0, 2 * Math.PI);
+        context.closePath();
+
+        context.fillStyle = "red"
+        context.fill();
+    }
+
+    drawText(context) {
+        context.fillStyle = "black";
+
+        var width = context.measureText(this.text).width;
+        var height = context.measureText("w").width; // this is a GUESS of height
+
+        context.fillText(this.text, this.x - (width / 2), this.y + (height / 2));
+    }
 }
