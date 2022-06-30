077901fb08289914da5f26d0fbb682306ee7ffca
daxmcpartland <ninnybobinny@outlook.com>
2022-06-29 22:57:31 -0600

    updated version

--- breadth-first-search.js
+++ breadth-first-search.js
@@ -1,6 +1,8 @@
 var numberOfNodes = 0;
 var nodes = [];
-
+var directional = false;
+var time = 100;
+var initalTime = 100;
 
 function createNode() {
     var c = document.getElementById("bfs-canvas");
@@ -17,10 +19,13 @@ function createNode() {
 }
 
 class Node {
+
     constructor(x, y, text) {
         this.x = x;
         this.y = y;
         this.text = text;
+        this.connectionList = [];
+        this.visitedState = false;
     }
 
     drawSelf(canvasContext) {
@@ -28,13 +33,15 @@ class Node {
         this.drawText(canvasContext);
     }
 
-    drawCircle(context) {
+    drawCircle(context, color) {
         context.beginPath();
 
         context.arc(this.x, this.y, 20, 0, 2 * Math.PI);
         context.closePath();
 
         context.fillStyle = "red";
+        if (color === true)
+            context.fillStyle = "blue";
         context.fill();
     }
 
@@ -57,13 +64,14 @@ class Node {
         context.fill();
     }
 
-    drawConnection(context, node) {
+    drawConnection(context, node, color) {
 
         var yDif = this.y - node.y;
         var xDif = node.x - this.x;
         var theta;
         var addPi = false;
         var thirdQuad = false;
+
 
 
         // If the position of the difference is in the 2nd quadrant we want to add pi after we find the angle
@@ -98,17 +106,25 @@ class Node {
         //Draw the line between nodes
         context.closePath();
         context.beginPath();
+
         context.moveTo(posX, posY);
         context.lineTo(posX2, posY2);
+
+        if (color === true)
+            context.strokeStyle = 'blue';
         context.stroke();
+        // drawArrow2(posX, posY, posX2, posY2, theta);
 
-
-
-
-
+        this.connectionList.push(node);
+        if (directional)
+            node.connectionList.push(this);
     }
 }
 
+/**
+ * TODO
+ * this function is useless if each node spawns away from other nodes
+ */
 function deleteLastNode() {
 
     if (nodes.length > 0) {
@@ -122,9 +138,7 @@ function deleteLastNode() {
     }
 }
 
-function getConnectionNodes() {
-
-
+function connectionNodes() {
     // Get the value of the first connection node
     var firstNode = nodes.at(document.getElementById("FirstNode").value);
     // Get the value of the second connection node
@@ -135,5 +149,73 @@ function getConnectionNodes() {
         var c = document.getElementById("bfs-canvas");
         var ctx = c.getContext("2d");
         firstNode.drawConnection(ctx, secondNode);
+
+    } else
+        throw console.error("Input correct nodes to add connections");
+}
+
+async function runBFS() {
+    var c = document.getElementById("bfs-canvas");
+    var ctx = c.getContext("2d");
+    var startingNode = nodes.at(document.getElementById("BFS").value);
+    if (typeof startingNode === 'undefined')
+        throw console.error("Input a node inorder to run BFS");
+
+    // Create the queue used to run a bfs
+    const nodeQueue = [];
+    nodeQueue.push(startingNode);
+    startingNode.drawCircle(ctx, true);
+    startingNode.drawText(ctx);
+    startingNode.visitedState = true;
+    // continue the bfs until there are no nodes left in the queue
+    while (nodeQueue.length > 0) {
+        var currentNode = nodeQueue.pop();
+        await sleep(time);
+        // Recolor the connections between each node
+        for (i = 0; i < currentNode.connectionList.length; i++) {
+            const connectionNode = currentNode.connectionList.at(i);
+            if (connectionNode.visitedState === false) {
+                connectionNode.visitedState = true;
+                nodeQueue.push(connectionNode);
+                redrawNodeConnection(ctx, currentNode, connectionNode);
+            }
+
+        }
+        await sleep(time);
+        // Recolor each node
+        for (i = 0; i < currentNode.connectionList.length; i++) {
+            const connectionNode = currentNode.connectionList.at(i);
+            redrawNode(ctx, currentNode, connectionNode);
+        }
     }
 }
+/**
+ * Used to stop the code for a certain amount of time
+ * @param {integer} ms 
+ * @returns 
+ */
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function redrawNode(context, currentNode, connectionNode) {
+    connectionNode.drawCircle(context, true);
+    connectionNode.drawText(context);
+}
+function redrawNodeConnection(context, currentNode, connectionNode) {
+    currentNode.drawConnection(context, connectionNode, true);
+}
+
+function isDirectional() {
+    directional = true;
+}
+
+/**
+ * Used to change animation speed
+ */
+function changeAnimationSpeed() {
+    var slider = document.getElementById("animationSlider");
+    time = 100 * (10 - slider.value);
+    initalTime = 100 * (10 - slider.value);
+}
+
